47ede0db50cc8904aad8a388e75593ca8097b5c0
Harshit Bhargava <harshit.bhargava@ttu.edu>
2022-07-21 16:55:33 +0530

    Login and register button added in App.js

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route, Link} from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login"
 import Register from "./pages/Register"
@@ -10,6 +10,23 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome!</h1>
+      New user? Please Rigister!
+
+      <Link to="/register">
+        <button type="button">
+              Register
+        </button>
+      </Link>
+
+      <br></br>
+
+      Already registered? Please login!
+      
+      <Link to="/login">
+        <button type="button">
+              Login
+        </button>
+      </Link>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />}/>
